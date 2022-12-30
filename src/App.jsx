e618215fcbf37873bad6f6e3258a0971832251dc
linxubin <905637031@qq.com>
2022-12-30 22:55:12 +0800

    feat: init

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+// import { useState } from 'react'
+
+import {
+  // Switch
+  // Transfer
+  // Collapse
+  // Calendar
+} from './components'
+
+function App() {
+
+  // const [checked, setChecked] = useState(false)
+
+  // const toggleChecked = () => {
+  //   setChecked(!checked)
+  // }
+
+  // const data = [
+  //   {
+  //     label: '选项1',
+  //     disabled: false,
+  //     value: false
+  //   },
+  //   {
+  //     label: '选项2',
+  //     disabled: false,
+  //     value: false
+  //   },
+  //   {
+  //     label: '选项3',
+  //     disabled: true,
+  //     value: false
+  //   },
+  //   {
+  //     label: '选项4',
+  //     disabled: false,
+  //     value: false
+  //   },
+  //   {
+  //     label: '选项5',
+  //     disabled: false,
+  //     value: false
+  //   },
+  //   {
+  //     label: '选项6',
+  //     disabled: true,
+  //     value: false
+  //   }
+  // ]
+
+  // const collapseList = [
+  //   {
+  //     title: '标题1',
+  //     name: '1',
+  //     content: '代码是写出来给人看的，附带能在机器上运行',
+  //     disabled: false
+  //   },
+  //   {
+  //     title: '标题2',
+  //     name: '2',
+  //     content: '技术无非就是那些开发它的人的共同灵魂',
+  //     disabled: false
+  //   },
+  //   {
+  //     title: '标题3',
+  //     name: '3',
+  //     content: '在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准',
+  //     disabled: true
+  //   },
+  //   {
+  //     title: '标题4',
+  //     name: '4',
+  //     content: '不会回头的东西有四件：说出口的话、离弦的箭、逝去的生活和失去的机会',
+  //     disabled: false
+  //   },
+  //   {
+  //     title: '标题5',
+  //     name: '5',
+  //     content: '米袋虽空——樱花开哉',
+  //     disabled: false
+  //   }
+  // ]
+
+  return (
+    <div 
+      className="App" 
+    >
+      {/* <Slider
+      /> */}
+      {/* <Switch
+        checked={ checked }
+        disabled={ false }
+        toggleChecked={ toggleChecked }
+      /> */}
+      {/* <Transfer
+        data={data}
+      /> */}
+      {/* <Collapse
+        data={ collapseList }
+        active={['1', '4']}
+      /> */}
+      {/* <Calendar /> */}
+    </div>
+  )
+}
+
+export default App
