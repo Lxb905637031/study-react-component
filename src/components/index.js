7e0fcc78f5ff93be0e8352153d27e2f755f87f47
linxubin <905637031@qq.com>
2022-12-30 22:59:07 +0800

    feat: nothing

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import { Slider } from './slider'
+import { Switch } from './switch'
+import { Transfer } from './transfer'
+import { Collapse } from './collapse'
+import { Calendar } from './calendar'
+
+export {
+    Slider,
+    Switch,
+    Transfer,
+    Collapse,
+    Calendar
+}
