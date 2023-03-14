e9f669f4bfad4e3e9ec0fa4906464bfd0d33710c
linxubin <905637031@qq.com>
2023-03-14 16:48:01 +0800

    feat: 删除audio

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,11 @@ import { Switch } from './switch'
 import { Transfer } from './transfer'
 import { Collapse } from './collapse'
 import { Calendar } from './calendar'
-import { Audio } from './audio'
 
 export {
     Slider,
     Switch,
     Transfer,
     Collapse,
-    Calendar,
-    Audio
+    Calendar
 }
